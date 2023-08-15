f94c4c14a52a6dc3c25ae9eae4cbe9613df30792
Dane knight <daneknight@danesmacstudio.myfiosgateway.com>
2023-08-15 12:54:22 -0400

    black jack game

---
+++ games.py
@@ -0,0 +1,78 @@
+import random
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+    
+    def __str__(self):
+        return f"{self.rank['rank']} of  {self.suit}"
+
+
+
+
+class Deck:
+    def __init__(self):
+        
+        self.cards = []
+        suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+        ranks = [
+                {"rank": "A", "value": 11},
+                {"rank": "2", "value": 2}, 
+                {"rank": "3", "value": 3}, 
+                {"rank": "4", "value": 4}, 
+                {"rank": "5", "value": 5}, 
+                {"rank": "6", "value": 6}, 
+                {"rank": "7", "value": 7}, 
+                {"rank": "8", "value": 8}, 
+                {"rank": "9", "value": 9}, 
+                {"rank": "10", "value": 10}, 
+                {"rank": "J", "value": 10}, 
+                {"rank": "Q", "value": 10}, 
+                {"rank": "K", "value": 10}
+            ]
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append([suit, rank])
+
+    def shuffle(self):
+        if len(self.cards) > 1:
+            random.shuffle(self.cards)              
+
+    def deal(self, number):
+        cards_dealt = []
+        for x in range(number):
+            if len(self.cards) > 0:
+                card = self.cards.pop()
+                cards_dealt.append(card)
+        return cards_dealt 
+ 
+card1 = Card("hearts", {"rank": "J", "value": 10})
+print(card1)
+
+
+
+
+
+
+
+
+
+
+
+# cards_dealt = deal(2)
+# card = cards_dealt[0]
+# rank = card[1]
+# if rank == "A":
+#     value = 11
+# elif rank == "J" or rank == "Q" or rank == "K":
+#     value = 10
+# else:
+#     value = rank
+
+# rank_dict = {"rank": rank, "value": value}
+
+# print(rank_dict["rank"], rank_dict["value"])
+
+# shuffle()
+# card = deal(1)[0]
+# print(card[1]["value"])
